012242c73138d1299db9c75e6903e7aa0b3d134f
Fabian Braun <fsbraun@gmx.de>
2025-07-18 07:09:21 +0200

    Cascade deletion of blog tables

--- djangocms_stories/migrations/0002_auto_20250618_1556.py
+++ djangocms_stories/migrations/0002_auto_20250618_1556.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations, models
 from django.db.migrations.recorder import MigrationRecorder
+from django.core.management import execute_from_command_line
 
 migration_table = (
     ("BlogConfig", "StoriesConfig"),
@@ -197,8 +198,16 @@ def migrate_from_blog_to_stories(apps, schema_editor):
     # 6. Drop all djangocms_blog tables from the database
     with schema_editor.connection.cursor() as cursor:
         tables = schema_editor.connection.introspection.table_names(cursor)
-        blog_tables = [t for t in tables if t.startswith("djangocms_blog_")]
-        for table in blog_tables:
+        blog_m2n_tables = [
+            t for t in tables if t.startswith("djangocms_blog_") and t.count("_") > 2
+        ]
+        blog_plugin_tables = [t for t in tables if t.startswith("djangocms_blog_") and t.endswith("plugin") and t.count("_") <= 2]
+        blog_other_tables = [t for t in tables if t.startswith("djangocms_blog_") and t not in blog_m2n_tables and t not in blog_plugin_tables]
+        for table in blog_m2n_tables:
+            cursor.execute(f'DROP TABLE IF EXISTS "{table}";')
+        for table in blog_plugin_tables:
+            cursor.execute(f'DROP TABLE IF EXISTS "{table}";')
+        for table in blog_other_tables:
             cursor.execute(f'DROP TABLE IF EXISTS "{table}";')
 
 
