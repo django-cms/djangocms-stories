5b394e3d0127e518e903d1b238a41eecf73814d8
Fabian Braun <fsbraun@gmx.de>
2025-07-19 00:36:45 +0200

    Update sitemap tests

--- tests/test_sitemap.py
+++ tests/test_sitemap.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.django_db
 def test_sitemap(page_with_menu, many_posts):
     """
     Tests if the sitemap contains the correct URLs for the posts and categories
@@ -11,3 +15,15 @@ def test_sitemap(page_with_menu, many_posts):
     for post in many_posts:
         absolute_url = post.get_absolute_url()
         assert not absolute_url or absolute_url in urls
+
+
+@pytest.mark.django_db
+def test_sitemap_priority_and_changefreq(page_with_menu, many_posts):
+    """Tests if the sitemap priority and changefreq are set correctly"""
+    from djangocms_stories.sitemaps import StoriesSitemap
+
+    sitemap = StoriesSitemap()
+    # Check frequencies and priorities
+    for item in sitemap.items():
+        assert sitemap.priority(item) == item.app_config.sitemap_priority
+        assert sitemap.changefreq(item) == item.app_config.sitemap_changefreq
