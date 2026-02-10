685f5bd7a136450312f3eb2eba07d3322b1debbc
Corentin Bettiol <45763865+corentinbettiol@users.noreply.github.com>
2026-02-10 16:47:34 +0100

    FIX: fix setting to disable unicode chars in slug fields (#81)

--- djangocms_stories/fields.py
+++ djangocms_stories/fields.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.utils.text import slugify as django_slugify
 
+from .settings import get_setting
+
 __all__ = ["slugify", "LanguageSelector"]
 
 
 def slugify(base):
-    return django_slugify(base, allow_unicode=True)
+    return django_slugify(base, allow_unicode=get_setting("ALLOW_UNICODE_SLUGS"))
 
 
 class LanguageSelector(forms.Select):
