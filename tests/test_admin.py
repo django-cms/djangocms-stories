607208badd3511e4d6a90bdc1a1892b8132c5f05
Fabian Braun <fsbraun@gmx.de>
2026-04-10 10:54:38 +0200

    fix: PostContent could return a lazy string instad of string (#94)
    * fix: PostContent in some cases returned a lazy object instead of a string
    
    * fix: PostContent could return a lazy string instad of string
    
    * fix: django 6.0 test

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -240,7 +240,7 @@ def test_post_change_admin(admin_client, default_config, assert_html_in_response
     )
 
     # Both post and post content fields are present
-    if DJANGO_VERSION >= (6,0):
+    if DJANGO_VERSION >= (6,1):
         assert_html_in_response('<legend class="inline" for="id_author">Author:</legend>', response)  # Post author field
     else:
         assert_html_in_response('<label class="inline" for="id_author">Author:</label>', response)  # Post author field
