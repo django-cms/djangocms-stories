486090e7e1da5ceda8c0ba484366e5ee8f8a5c3b
Fabian Braun <fsbraun@gmx.de>
2026-06-25 21:40:49 +0200

    chore: Remove unused file

--- djangocms_stories/taggit_urls.py
+++
@@ -1,6 +0,0 @@
-# This file is used on divio cloud only during automatic setup
-from django.urls import include, path  # pragma: no cover
-
-urlpatterns = [  # pragma: no cover
-    path("", include("djangocms_stories.tag_autosuggest")),
-]
